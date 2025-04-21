8bea2b702fb15dc34a1be6299ed8f70283a228bf
binary3300 <https://github.com/binary3300/SpringBoot2025.git>
2025-04-21 18:24:11 +0900

    2025-04-21 희망도서신청페이지에서 검색하면 동일화면에서 검색결과가 나오게 만들었다. 오전에 배운걸 그대로 응용한것. 오류나서 안나오기는 하지만 그건 내일 고치면 됨

--- 40db/src/main/java/com/db40/library/binary3300/BookHopeController.java
+++ 40db/src/main/java/com/db40/library/binary3300/BookHopeController.java
@@ -1,11 +1,14 @@
 package com.db40.library.binary3300;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 
@@ -39,11 +42,24 @@ public class BookHopeController {
 		 return "/hopeBook/hope_detail"; 
 	}
 	
+	// 검색시 타이틀로 불러옴
+	@GetMapping(value="/hopeBook/hope_usersearch/{search}", produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String hope_usersearch( @PathVariable String search) throws IOException {
+		System.out.println("실행");
+		return  bookHopeApi.getApi(search);
+	}
 	
+//	@GetMapping(value = "/hopeBook/hope_usersearch/{search}", produces = "application/json;charset=UTF-8")
+//	@ResponseBody
+//	public String booksJson( @PathVariable String search ) throws IOException {
+//		return bookHopeApi.getApi(search);
+//	}
+//	
 	 
-
-	// 희망도서 등록하기 데이터베이스에 도서추가기능...
-
-	// 희망도서 반려하기 업데이트
+	// 희망도서 반려하기/등록하기 업데이트에서 하나로 될거같음 (이건 관리자만 쓸수있는기능)
+	//book_hope_no
+	
+	
 
 }
